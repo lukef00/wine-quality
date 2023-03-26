e1377030c448122f7653ce49188d029257740de1
Łukasz Flisak <lu.dev00@gmail.com>
2023-03-26 13:45:38 +0200

    Add tabs and statistical data

---
+++ gui.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+import pandas as pd
+
+import tabs
+
+DATASET = None
+
+class StatusBar(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.label = tk.Label(self)
+        self.label.pack(side=tk.LEFT)
+        self.pack(side=tk.BOTTOM, fill=tk.X)
+
+    def set(self, newText, color):
+        self.label.config(text=newText, fg=color)
+
+    def clear(self):
+        self.label.config(text="")
+
+
+class App(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.create_menu(master)
+        self.create_status_bar(master)
+        self.create_tabs(master)
+
+        self.add_keybinds(master)
+        self.pack()
+
+
+    def create_menu(self, master):
+        self.menu = Menu(master)
+        file_menu = Menu(self.menu, tearoff = 0)
+        file_menu.add_command(label = "Load dataset", command=self.load_dataset)
+        file_menu.add_separator()
+        file_menu.add_command(label = "Exit", command = master.destroy)
+        self.menu.add_cascade(label = "File", menu = file_menu)
+        self.master.config(menu = self.menu)
+
+
+    def create_status_bar(self, master):
+        self.status = StatusBar(master)
+        self.status.set("Dataset not loaded", '#ff0000')
+
+
+    def create_tabs(self, master):
+        self.tabController = ttk.Notebook(master)
+        self.statistics = tabs.StatsTab(self.tabController)
+        # TODO: change lines below into classes and place them in tabs.py,
+        self.plots = ttk.Frame(self.tabController)
+        self.model = ttk.Frame(self.tabController)
+
+        self.tabController.add(self.statistics, text="Statistical data")
+        self.tabController.add(self.plots, text="Plots")
+        self.tabController.add(self.model, text="Model")
+
+        self.tabController.pack(expand = 1, fill='both')
+
+
+    def add_keybinds(self, master):
+        master.bind('<Control-o>', self.load_dataset)
+
+
+    def load_dataset(self, event):
+        filetypes = (
+                ('csv', '*.csv'),
+                ('text files', '*.txt'),
+                ('All files', '*.*')
+                )
+
+        filename = filedialog.askopenfilename(
+                title='Open a file',
+                filetypes=filetypes)
+
+        DATASET = pd.read_csv(filename, sep=";")
+        self.status.set("Dataset loaded", '#00ff00')
+        self.statistics.set_data_frame(DATASET)
+
+
+
+
+root = tk.Tk()
+myapp = App(root)
+myapp.mainloop()
---
+++ tabs.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+import pandas as pd
+
+
+class StatsTab(ttk.Frame):
+    def __init__(self, master):
+        self.df = None
+        self.frame = ttk.Frame.__init__(self, master)
+        tk.Label(self, text="Stats Page", font=('bold')).pack()
+        self.prepare_table()
+
+
+    def set_data_frame(self, df):
+        if not self.df:
+           self.table_frame.pack()
+
+        self.df = df
+        self.column_names = df.columns.values.tolist()
+        self.calculate()
+
+
+    def calculate(self):
+        min_vals = self.df.min()
+        max_vals = self.df.max()
+        mean_vals = self.df.mean()
+        median_vals = self.df.median()
+        mode_vals = self.df.mode()
+        std_vals = self.df.std()
+
+        for key in min_vals.keys():
+            self.tree.insert('', 
+                             tk.END, 
+                             values=(
+                                 key,
+                                 min_vals[key],
+                                 max_vals[key],
+                                 round(mean_vals[key], 5),
+                                 median_vals[key],
+                                 mode_vals[key].at[0],
+                                 round(std_vals[key], 5),
+                                 )
+                             )
+
+
+    def prepare_table(self):
+        self.table_frame = Frame(self)
+        scrollbar = Scrollbar(self.table_frame)
+        scrollbar.pack(side = RIGHT, fill = Y)
+        self.tree = ttk.Treeview(
+                self.table_frame,
+                yscrollcommand=scrollbar.set,
+                columns = ('column', 'min', 'max', 'mean', 'median', 'mode', 'standard deviation'),
+                show = 'headings'
+                )
+
+        for col in self.tree['columns']:
+            self.tree.heading(col, text = col)
+            self.tree.column(col, width = 80, anchor = 'e')
+
+        self.tree.column('column', width = 150, anchor = 'w')
+        self.tree.column('standard deviation', width = 150)
+        scrollbar.config(command=self.tree.yview)
+
+        self.tree.pack()
